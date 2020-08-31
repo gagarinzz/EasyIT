fcf8111a5f5822da7e8951e44d6535c368c63967
Admin <gfdgfdg@i.ua>
2020-08-31 20:19:20 +0300

    Edited1

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -4,8 +4,8 @@ import com.company.task2.Human;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human( "Dima",13, 1.8, 66);
-        human.say();
+        Human human1 = new Human( "Dima",13, 1.8, 66);
+        human1.say();
     }
 }
 
