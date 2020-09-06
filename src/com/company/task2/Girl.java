04d989c400f690053dd6973f0d053aaa543ef90c
Юра <DFg@hdhfhg.ru>
2020-09-06 15:23:21 +0300

    Edited5

--- src/com/company/task2/Girl.java
+++ src/com/company/task2/Girl.java
@@ -3,11 +3,26 @@ package com.company.task2;
 public class Girl extends Human {
     private Hobby hobby;
 
-    public Girl(String name, int age, double height, double weith, Hobby hobby) {
-        super(name, age, height, weith);
+    public Girl(String name, int age, double height, double weigth) {
+        super(name, age, height, weigth);
         this.hobby = hobby;
     }
-    public Hobby getHobby(){
+    public Hobby getHobby() {
         return hobby;
     }
-}
+
+        class Friend extends Human {
+            private Hobby hobby;
+
+            public Friend(String name, int age, double height, double weigth, Hobby hobby) {
+                super(name, age, height, weigth);
+                this.hobby = hobby;
+            }
+
+            public Hobby getHobby() {
+                return hobby;
+            }
+
+        }
+    }
+
